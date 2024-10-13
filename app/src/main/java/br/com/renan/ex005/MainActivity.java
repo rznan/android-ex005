2086692cd0e48ab94ea7fd8d1d6184b8be4a2b6a
Renan <109163037+rznan@users.noreply.github.com>
2024-10-12 22:18:47 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/renan/ex005/MainActivity.java
@@ -0,0 +1,46 @@
+package br.com.renan.ex005;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+/*
+*@author: renan santos carvalho
+*/
+public class MainActivity extends AppCompatActivity {
+
+    EditText consume;
+    EditText current;
+    TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        consume = findViewById(R.id.consume);
+        current = findViewById(R.id.currentGas);
+        result = findViewById(R.id.result);
+
+        findViewById(R.id.button).setOnClickListener(e -> calculate());
+    }
+
+    private void calculate() {
+        double consume = Double.parseDouble(String.valueOf(this.consume.getText()));
+        double gas = Double.parseDouble(String.valueOf(this.current.getText()));
+        result.setText(String.valueOf((consume * gas) * 1000 ));
+    }
+}
